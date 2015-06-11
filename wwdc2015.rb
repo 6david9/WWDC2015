b1f9b3c8c83bfcccefc0f54e652535a26fbd8a06
leiyan <leiyan@douban.com>
2015-06-11 14:30:17 +0800

    add download script

---
+++ wwdc2015.rb
@@ -0,0 +1,43 @@
+require 'open-uri'
+require 'nokogiri'
+require 'thread'
+
+base_url = 'https://developer.apple.com/videos/wwdc/2015/'
+url_links = {}
+
+doc = Nokogiri::HTML(open(base_url))
+links = doc.css('section.video_sections li a')
+links.each do |link|
+  title = link.text
+  url_str = URI.join(base_url, link.attributes['href'].value).to_s
+  url_links[title] = url_str
+end
+
+semaphore = Mutex.new
+trdgroup = ThreadGroup.new
+
+sd_links = {}
+hd_links = {}
+
+url_links.each_pair do |title, link|
+  trd = Thread.new {
+  	doc = Nokogiri::HTML(open(link))
+  	sd_link = doc.xpath("//a[text()='SD']").last.attributes['href'].value
+  	hd_link = doc.xpath("//a[text()='HD']").last.attributes['href'].value
+
+  	semaphore.synchronize {
+  		puts "#{title}:  \n\tSD:#{sd_link}\n\tHD:#{hd_link}"
+  		sd_links[title] = sd_link
+  		hd_links[title] = hd_link
+ 	  }
+  }
+  trdgroup.add(trd)
+end
+
+loop do
+	break if trdgroup.list.size == 0
+	sleep 1
+end
+
+File.open('wwdc2015-sd.txt', 'w+') { |f| f.write(sd_links.values.join("\n")) }
+File.open('wwdc2015-hd.txt', 'w+') { |f| f.write(hd_links.values.join("\n")) }
